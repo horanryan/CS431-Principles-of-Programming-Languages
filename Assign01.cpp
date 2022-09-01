9497cc2cad1463507ecbd2f062080c37c351f0f9
Ryan Horan <rhoran27@gmail.com>
2022-09-01 14:36:39 -0400

    Working Assignment 1

--- Assign01.cpp
+++ Assign01.cpp
@@ -7,27 +7,22 @@ CS431-Principles of Programming Languages
 
 using namespace std; 
 
-void INTDIV(int N, int D, int Q, int R);
-
-
 int main(int argc, char **argv){
     //Get two integers from user
     int N,D,R,Q;
     cout << " Enter two positive integers to divide: ";
     cin >> N >> D;
-    INTDIV(N,D,Q, R);
-    
-
-    cout << N << '\n' << D << '\n' << Q << '\n' << R << endl;
-    return 0;
-}
-void INTDIV(int N, int D, int Q, int R){
-     Q = 0;
+    Q = 0;
     while(N >= D){
         N -= D;
         Q += 1;       
-    }
-    
+    }    
     R = N;
-    return;
+    
+
+    cout << "Numerator: " << N << '\n'; 
+    cout << "Denominator: "<< D << '\n';
+    cout <<  "Quotient: " << Q << '\n';
+    cout << "Remainder: " << R << '\n';
+    return 0;
 }
