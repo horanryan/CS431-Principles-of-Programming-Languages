06d812b274be184b2d16e7c0864d74101109c808
Ryan Horan <rhoran27@gmail.com>
2022-09-13 10:48:46 -0400

    ..

--- Assign01.cpp
+++ Assign01.cpp
@@ -11,45 +11,27 @@ Integer Division to show how global variables can rot
 using namespace std; 
 
 //Global Variables
-<<<<<<< HEAD
 int N;
 int D;
 int R;
 int Q;
 
-void INTDIV(){
-     Q = 0;
-=======
-int N,D,R,Q;
 
 void INTDIV(){
     Q = 0;
->>>>>>> f4a1488ae9360f023b313db0c22c4743507ce6c9
     while(N >= D){
         N -= D;
         Q += 1;       
     }    
-<<<<<<< HEAD
     R = N;  
 
-}
-
-int main(int argc, char **argv){
-    //Get two integers from user
-    cout << " Enter two positive integers to divide: ";
-    cin >> N >> D;
-    INTDIV();    
-=======
-    R = N;    
 }
 
 int main(int argc, char **argv){
     //Get two integers from user   
     cout << " Enter two positive integers to divide: ";
     cin >> N >> D;
-    INTDIV();
-    
->>>>>>> f4a1488ae9360f023b313db0c22c4743507ce6c9
+    INTDIV();    
 
     cout << "Numerator: " << N << '\n'
          << "Denominator: "<< D << '\n'
