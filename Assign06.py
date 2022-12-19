ab11f68bc4381b4120ebbe82748abcb87e531e9b
Ryan Horan <horanr@sunypoly.edu>
2022-12-18 20:01:10 -0500

    completed

--- Assign06.py
+++ Assign06.py
@@ -1,17 +1,34 @@
+"""
+Ryan Horan
+December 9,2022
+Python 3.10
+Accepts up to 30 values from the keyboard for each of two variables (use arrays, vectors or
+lists as appropriate), then compute and print the following for each of the two sequences:
+a. the number of entries
+b. the minimum value
+c. the maximum value
+d. the range
+e. the arithmetic mean
+f. the variance
+g. the standard deviation  
+    
+"""
+
 import statistics
 
-array = []
-
+list = []
+#keep entering numbers until 30 inputs is entered
 for i in range(30):
-    n = int(input("enter an integer or enter -1 to exit: "))
+    n = int(input("enter an integer or enter -1 to stop entering integers: "))
     if n == -1:
         break    
-    array.append(n)
-print("\nNumber of entries:\t",len(array),
-          "\nMin value:\t",min(array),
-          "\nMax value:\t",max(array),
-          "\nRange:\t",min(array)," - ",max(array),
-          "\nArithmetic Mean:\t",statistics.mean(array),
-          "\nVariance:\t",statistics.pvariance(array),
-          "\nStd. Deviation:\t",statistics.pstdev(array)
+    list.append(n)
+#calculate and print data
+print("\nNumber of entries:\t",len(list),
+          "\nMin value:\t",min(list),
+          "\nMax value:\t",max(list),
+          "\nRange:\t",max(list)-min(list),
+          "\nArithmetic Mean:\t",statistics.mean(list),
+          "\nVariance:\t",statistics.pvariance(list),
+          "\nStd. Deviation:\t",statistics.pstdev(list)
           )
